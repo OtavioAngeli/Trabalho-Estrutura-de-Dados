363b6f68dbbbd1e6a36bd16648e208b74a5f4a29
Otavio Angeli <otavio.miguel.angeli@gmail.com>
2017-09-05 21:33:16 -0300

    Lista Encadeada

--- trabalho.c
+++ trabalho.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/********************** PILHA ENCADEADA **********************/
 struct Pilha {
 	int topo; /* posição elemento topo */
 	int capa;
 	float *prox;
+};
+
+/********************** FILA ENCADEADA **********************/
+struct Fila {
+	int capacidade;
+	float *dados;
+	int primeiro;
+	int ultimo;
+	int nItens; 
 };
 
 /******************************* MENU *******************************/
@@ -37,7 +47,11 @@ void imprimeMenuPilhaEncadeada(){
 }
 
 void imprimeMenuFilaEncadeada(){
-	
+	printf("\n\t--- FILA ENCADEADA ---\n");
+	printf("1. Inserir elemento\n");
+	printf("2. Remover elemento\n");
+	printf("3. Mostrar Fila\n");
+	printf("0. Sair\n> ");
 }
 
 /******************************* FUNÇÕES PILHA SEQUENCIAL*******************************/
@@ -141,8 +155,103 @@ void pilhaEncadeada(){
 /******************************* FILA SEQUENCIAL *******************************/
 
 /******************************* FUNÇÕES FILA ENCADEADA *******************************/
+void criarFila( struct Fila *f, int c ) { 
+	f->capacidade = c;
+	f->dados = (float*) malloc (f->capacidade * sizeof(float));
+	f->primeiro = 0;
+	f->ultimo = -1;
+	f->nItens = 0; 
+}
+
+void inserir(struct Fila *f, int v) {
+	if(f->ultimo == f->capacidade-1)
+		f->ultimo = -1;
+	f->ultimo++;
+	f->dados[f->ultimo] = v; // incrementa ultimo e insere
+	f->nItens++; // mais um item inserido
+}
+
+int remover( struct Fila *f ) { // pega o item do começo da fila
+	int temp = f->dados[f->primeiro++]; // pega o valor e incrementa o primeiro
+	if(f->primeiro == f->capacidade)
+		f->primeiro = 0;
+	f->nItens--;  // um item retirado
+	return temp;
+}
+
+int estaVazia( struct Fila *f ) { // retorna verdadeiro se a fila está vazia
+	return (f->nItens==0);
+}
+
+int estaCheia( struct Fila *f ) { // retorna verdadeiro se a fila está cheia
+	return (f->nItens == f->capacidade);
+}
+
+void mostrarFila(struct Fila *f){
+	int cont, i;
+	for ( cont=0, i= f->primeiro; cont < f->nItens; cont++){
+		printf("%.2f\t",f->dados[i++]);
+		if (i == f->capacidade)
+			i=0;
+	}
+	printf("\n\n");
+}
 
 /******************************* FILA ENCADEADA *******************************/
+void filaEncadeada(){
+	int opcao, capa;
+	float valor;
+	struct Fila fEncadeada;
+
+	// criar a fila
+	printf("\nCapacidade da fila? ");
+	scanf("%d",&capa);
+
+	criarFila(&fEncadeada, capa);
+
+	while( 1 ){	
+		imprimeMenuFilaEncadeada();
+		scanf("%d", &opcao);
+		
+		switch(opcao){
+			case 0: 
+				exit(0);
+			case 1:
+			// insere elemento
+				if (estaCheia(&fEncadeada)){
+					printf("\nFila Cheia!!!\n\n");
+				}else {
+					printf("\nValor do elemento a ser inserido? ");
+					scanf("%f", &valor);
+					inserir(&fEncadeada,valor);
+				}
+				break;
+		
+			case 2:
+			// remove elemento
+				if (estaVazia(&fEncadeada)){
+					printf("\nFila vazia!!!\n\n");
+				}else {
+					valor = remover(&fEncadeada);
+					printf("\n%1f removido com sucesso\n\n", valor) ; 
+				}
+				break;
+		
+			case 3:
+			// mostrar fila
+				if (estaVazia(&fEncadeada)){
+					printf("\nFila vazia!!!\n\n");
+				}else {
+					printf("\nConteudo da fila => ");
+					mostrarFila(&fEncadeada);
+				}
+				break;
+		
+			default:
+				printf("\nOpcao Invalida\n\n");
+		}
+	}
+}
 
 //----------------------------------  MAIN ---------------------------------- //
 int main(){
@@ -171,8 +280,7 @@ int main(){
 				break;
 			case 4:	
 			/**************** FILA ENCADEADA ****************/
-				printf("Fila Encadeada\n");
-				imprimeMenuFilaEncadeada();
+				filaEncadeada();
 				break;
 			default:
 				printf("Opção invalida.\n");
